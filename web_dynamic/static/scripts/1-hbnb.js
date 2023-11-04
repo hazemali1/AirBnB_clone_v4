4f4065de6bdf14e3df3cfd5228bd3a51298d63b0
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-11-04 16:07:14 +0200

    checkbox

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
     const amny_id = {};
     $('input[type="checkbox"]').change(function(){
-        const checkbox = $(this);
         if ($(this).is(':checked')){
-            amny_id[checkbox.data('id')] = checkbox.data('name')
+            amny_id[$(this).data('data-id')] = $(this).data('data-name')
         }else{
-            delete amny_id[checkbox.data('id')];
+            delete amny_id[$(this).data('data-id')];
         }
     });
 });
