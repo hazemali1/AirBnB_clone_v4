712de92538a7ad5515155f2593aab1f2e04de04d
beboo22 <mt557829@gmail.com>
2023-11-04 20:43:46 +0200

    task

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -26,14 +26,31 @@ $.ajax({
     success: function (data) {
     for(let i = 0; i < data.length;i++){
       let place = data[i];
-      $('.places ').append('<article><div class="title_box"><h2>'+ place.name +
+      $('.places ').text('<article><div class="title_box"><h2>'+ place.name +
       '</h2><div class="price_by_night">$'+ place.price_by_night +
       '</div></div><div class="information"><div class="max_guest">'+ place.max_guest +
       '</div><div class="number_rooms">'+ place.number_rooms +
       '</div><div class="number_bathrooms">'+ place.number_bathrooms +
-      '</div></div><div class="description">'+ place.description + '</div></article>'
-    );
-    
-    }      
-  }, 
+      '</div></div><div class="description">'+ place.description + '</div></article>')
+    }}});
+$('button').click(function() {
+    $.ajax({ 
+        type: 'POST',
+        url: 'http://0.0.0.0:5001/api/v1/places_search',
+        data: JSON.stringify(Object.keys(amny_id)),
+        contentType: 'application/json',
+        success: function (data) {
+        for(let i = 0; i < data.length;i++){
+          let place = data[i];
+          $('.places ').text('<article><div class="title_box"><h2>'+ place.name +
+          '</h2><div class="price_by_night">$'+ place.price_by_night +
+          '</div></div><div class="information"><div class="max_guest">'+ place.max_guest +
+          '</div><div class="number_rooms">'+ place.number_rooms +
+          '</div><div class="number_bathrooms">'+ place.number_bathrooms +
+          '</div></div><div class="description">'+ place.description + '</div></article>')
+        }
+    }
 });
+
+}); 
+
