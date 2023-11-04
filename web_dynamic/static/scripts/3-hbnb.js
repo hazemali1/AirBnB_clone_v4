6ea81ad7792f25af8f37ea1cdafcf132c30c3ac8
beboo22 <mt557829@gmail.com>
2023-11-04 19:08:21 +0200

    task

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -19,14 +19,21 @@ $(document).ready(function(){
 });
 
 $.ajax({ 
-  type: 'POST',
-  url: 'http://0.0.0.0:5001/api/v1/places_search',
-  Content-Type: 'application/json',
-  data : '{}',
-  success: function(data) {
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    data: '{}',
+    contentType: 'application/json',
+    success: function (data) {
     for(let i = 0; i < data.length;i++){
       let place = data[i];
-      $('.places ').append('<article><h2>'+ place.name +'</h2></article>');
+      $('.places ').append('<article><div class="title_box"><h2>'+ place.name +
+      '</h2><div class="price_by_night">$'+ place.price_by_night +
+      '</div></div><div class="information"><div class="max_guest">'+ place.max_guest +
+      '</div><div class="number_rooms">'+ place.number_rooms +
+      '</div><div class="number_bathrooms">'+ place.number_bathrooms +
+      '</div></div><div class="description">'+ place.description + '</div></article>'
+    );
+    
     }      
   }, 
 });
