85607431a5bbf8d0234bc82b4f6816e5c2fffb37
beboo22 <mt557829@gmail.com>
2023-11-04 18:33:19 +0200

    task

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    const amny_id = {};
+    $('input[type="checkbox"]').change(function(){
+        if ($(this).is(':checked')){
+            amny_id[$(this).data('id')] = $(this).data('name')
+        }else{
+            delete amny_id[$(this).data('id')];
+        }
+				let h4 = Object.values(amny_id);
+				$('.amenities h4').text(h4.join(', '));
+    });
+		$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
+
+$.ajax({ 
+  url: 'http://0.0.0.0:5001/api/v1/places_search',
+  Content-Type: 'application/json',
+  data : '{}',
+  method: 'GET', 
+  success: function(data) {
+    for(let i = 0; i < data.length;i++){
+      let place = data[i];
+      $('.places ').append('<article><h2>'+ place.name +'</h2></article>');
+    }      
+  }, 
+});
