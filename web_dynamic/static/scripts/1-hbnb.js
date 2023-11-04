902b898eef47932eb42f10cdff8f1afcc0888a03
beboo22 <mt557829@gmail.com>
2023-11-04 15:05:16 +0200

    task

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
     const amny_id = {};
     $('input[type="checkbox"]').change(function(){
+        const checkbox = $(this);
         if ($(this).is(':checked')){
-            const checkbox = $(this);
-
             amny_id[checkbox.data('id')] = checkbox.data('name')
         }else{
             delete amny_id[checkbox.data('id')];
